9b90d688a254b884fabd0a10a35bfa86bfa69c4b
feiyang <1142097478@qq.com>
2024-12-29 16:53:57 +0800

    删除build文件夹

--- build/opencv_description/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/elessar/russ_ws/ws7/install/opencv_description'
--- build/ur/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/elessar/russ_ws/ws7/src/ur
-# Build directory: /home/elessar/russ_ws/ws7/build/ur
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
